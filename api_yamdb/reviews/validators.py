db424b9badbe0e6136c0d014b3d9bd1fa580cf88
Kirill Maksimov <kamaksimov1410@gmail.com>
2023-04-03 21:21:48 +0300

    fix

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -3,5 +3,4 @@ from django.utils import timezone
 
 
 def max_value_timezone_now(value):
-    message = f"Год выпуска не может быть позже {timezone.now().year}"
     return MaxValueValidator(timezone.now().year)(value)
