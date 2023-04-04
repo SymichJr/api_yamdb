a2d07500a4222c98ae6a9645680119664bae6b6c
Kirill Maksimov <kamaksimov1410@gmail.com>
2023-04-05 00:31:56 +0300

    Review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -11,7 +11,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 
-from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
